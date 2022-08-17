e560b61bd027b8d92b8e7c63f8125fcc9335d727
jenambo <jenambo09@gmail.com>
2022-08-17 18:00:38 -0500

    added computer choice, player choice, and game modules

---
+++ rockpaperscissors.js
@@ -0,0 +1,97 @@
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+    return computerSelection;
+}
+
+function getPlayerSelection() {
+    let p1Choice = prompt("Please choose one: rock, paper, or scissors");
+    let playerSelection = p1Choice.toLowerCase();
+    return playerSelection;
+}
+
+// function playRound(computerSelection, playerSelection){
+//     let message = "";
+//     computerWins = 0;
+//     playerWins = 0;
+//     if (computerSelection === "rock" && playerSelection === "rock") {
+//         message += "It's a Tie!";
+//     } else if (computerSelection === "rock" && playerSelection === "scissors") {
+//         message += "Computer Wins!";
+//         computerWins = computerWins + 1;
+//     } else if (computerSelection === "rock" && playerSelection === "paper") {
+//         message += "Player Wins!";
+//         playerWins = playerWins + 1;
+//     } else if (computerSelection === "scissors" && playerSelection === "rock") {
+//         message += "Player Wins!";
+//         playerWins = playerWins + 1;
+//     } else if (computerSelection === "scissors" && playerSelection === "paper") {
+//         message += "Computer Wins!";
+//         computerWins = computerWins + 1;
+//     } else if (computerSelection === "scissors" && playerSelection === "scissors") {
+//         message += "Its a tie!";
+//     } else if (computerSelection === "paper" && playerSelection === "rock") {
+//         message += "Computer Wins!";
+//         computerWins = computerWins + 1;
+//     } else if (computerSelection === "paper" && playerSelection === "scissors") {
+//         message += "Player Wins!";
+//         playerWins = playerWins + 1;
+//     } else if (computerSelection === "paper" && playerSelection === "paper") {
+//         message += "Its a Tie!";
+//     } else {
+//         message += "Everyone loses!";
+//     }
+//     return {message, computerSelection, playerSelection, playerWins, computerWins};
+// }
+
+// console.log(playRound(getComputerChoice(),getPlayerSelection()))
+
+function game(){
+    computerWins = 0;
+    playerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        function playRound(computerSelection, playerSelection){
+            let message = "";
+            if (computerSelection === "rock" && playerSelection === "rock") {
+                message += "It's a Tie!";
+            } else if (computerSelection === "rock" && playerSelection === "scissors") {
+                message += "Computer Wins!";
+                computerWins = computerWins + 1;
+            } else if (computerSelection === "rock" && playerSelection === "paper") {
+                message += "Player Wins!";
+                playerWins = playerWins + 1;
+            } else if (computerSelection === "scissors" && playerSelection === "rock") {
+                message += "Player Wins!";
+                playerWins = playerWins + 1;
+            } else if (computerSelection === "scissors" && playerSelection === "paper") {
+                message += "Computer Wins!";
+                computerWins = computerWins + 1;
+            } else if (computerSelection === "scissors" && playerSelection === "scissors") {
+                message += "Its a tie!";
+            } else if (computerSelection === "paper" && playerSelection === "rock") {
+                message += "Computer Wins!";
+                computerWins = computerWins + 1;
+            } else if (computerSelection === "paper" && playerSelection === "scissors") {
+                message += "Player Wins!";
+                playerWins = playerWins + 1;
+            } else if (computerSelection === "paper" && playerSelection === "paper") {
+                message += "Its a Tie!";
+            } else {
+                message += "Everyone loses!";
+            }
+            return {message, computerSelection, playerSelection, playerWins, computerWins};
+        } 
+        
+        console.log(playRound(getComputerChoice(),getPlayerSelection()))  
+     }
+
+}
+console.log(game())
+
+// let champion = "";
+// if (computerWins > playerWins) {
+//     champion += "The Computer is the Champion!"
+// } else {
+//     champion += "You are the Champion!"
+// } return champion; 
