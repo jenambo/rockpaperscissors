4d403243e39524fc64b3e4b67e436bdb17d41b67
jenambo <jenambo09@gmail.com>
2022-08-24 11:30:50 -0500

    added working buttons ands win outputs

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,25 +1,31 @@
 let computerWins = 0;
 let playerWins = 0;
 
+const rock = document.querySelector("#p1Rock")
+const paper = document.querySelector("#p1Paper")
+const scissors = document.querySelector("#p1Scissors")
+let poneW = document.querySelector("#poneW")
+let alphaW = document.querySelector("#alphaW")
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let computerSelection = choices[Math.floor(Math.random()*choices.length)];
     return computerSelection;
-    
 }
 
+// function getPlayerSelection() {
+//     let p1Choice = prompt("Please choose one: rock, paper, or scissors");
+//     let playerSelection = p1Choice.toLowerCase();
+//     return playerSelection;
+// }
 
-function getPlayerSelection() {
-    let p1Choice = prompt("Please choose one: rock, paper, or scissors");
-    let playerSelection = p1Choice.toLowerCase();
-    return playerSelection;
-}
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(getComputerChoice(),getPlayerSelection()));
-     } 
-}
+// function game(){
+//     for (let i = 0; i < 5; i++) {
+//         console.log(playRound(getComputerChoice(),getPlayerSelection()));
+//      } 
+// }
 
 function playRound(computerSelection, playerSelection){
     let message = "";
@@ -28,43 +34,60 @@ function playRound(computerSelection, playerSelection){
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
         message += "Computer Wins!";
         computerWins = computerWins + 1;
+        alphaW.textContent = computerWins
     } else if (computerSelection === "rock" && playerSelection === "paper") {
         message += "Player Wins!";
         playerWins = playerWins + 1;
+        poneW.textContent = playerWins
     } else if (computerSelection === "scissors" && playerSelection === "rock") {
         message += "Player Wins!";
         playerWins = playerWins + 1;
+        poneW.textContent = playerWins
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
         message += "Computer Wins!";
         computerWins = computerWins + 1;
+        alphaW.textContent = computerWins
     } else if (computerSelection === "scissors" && playerSelection === "scissors") {
         message += "Its a tie!";
     } else if (computerSelection === "paper" && playerSelection === "rock") {
         message += "Computer Wins!";
         computerWins = computerWins + 1;
+        alphaW.textContent = computerWins
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
         message += "Player Wins!";
         playerWins = playerWins + 1;
+        poneW.textContent = playerWins
     } else if (computerSelection === "paper" && playerSelection === "paper") {
         message += "Its a Tie!";
     } else {
         message += "Everyone loses!";
     }
-    return [computerSelection, playerSelection, message, computerWins, playerWins];
+    console.log(computerSelection, playerSelection, message, computerWins, playerWins);
 
 }
 
-function winner() {
-    let champion = "";
-    if (computerWins > playerWins){
-        champion += "Computer is the Champion!";
-    } else if (computerWins < playerWins) {
-        champion += "The Human is the Champion!";
-    } else {
-        champion += "Wow, it's a tie!";
-    }
-    return champion;
-}
+// function winner() {
+//     let champion = "";
+//     if (computerWins > playerWins){
+//         champion += "Computer is the Champion!";
+//     } else if (computerWins < playerWins) {
+//         champion += "The Human is the Champion!";
+//     } else {
+//         champion += "Wow, it's a tie!";
+//     }
+//     return champion;
+// }
 
-game();
-console.log(winner());
+rock.addEventListener("click", e =>{
+    playRound(getComputerChoice(), "rock")
+});
+
+paper.addEventListener("click", e => {
+    playRound(getComputerChoice(), "paper")
+})
+
+scissors.addEventListener("click", e => {
+    playRound(getComputerChoice(), "scissors")
+})
+
+
